33fb34409fdb783fce65fcdbf45ee57f10a77d01
Marc Arcedo <arcedo.marc@gmail.com>
2025-05-27 20:17:22 +0200

    [ADD] query micro service project structure

---
+++ query-ms/eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: process.cwd(),
+      },
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+    ignores: ["jest.config.cjs"],
+  },
+]);
