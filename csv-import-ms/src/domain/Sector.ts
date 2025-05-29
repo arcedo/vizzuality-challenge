ec83e9eaf1ce0a1fb5dd993d6ffd512632bc11b7
Marc Arcedo <arcedo.marc@gmail.com>
2025-05-29 23:06:15 +0200

    [FIX] now sector values are stored correctly

--- csv-import-ms/src/domain/Sector.ts
+++ csv-import-ms/src/domain/Sector.ts
@@ -1,8 +1,8 @@
 export class Sector {
   public constructor(
     public readonly id: string,
-    public readonly name: string,
     public readonly country: string,
+    public readonly name: string,
     public readonly parentSector: string | null,
   ) {}
 }
