2853e24767cab51cb626495ac834edd9148227f9
Marc Arcedo <arcedo.marc@gmail.com>
2025-06-01 20:00:37 +0200

    [FIX] now returning proper errors on get status

--- query-ms/src/application/GetStatusUseCase.ts
+++ query-ms/src/application/GetStatusUseCase.ts
@@ -1,5 +1,6 @@
 import { StatusRepository } from "./ports/StatusRepository";
 import { Status } from "../domain/Status";
+import { StatusError } from "../domain/errors/StatusError";
 
 export class GetStatusUseCase {
   constructor(private readonly statusRepository: StatusRepository) {}
@@ -8,7 +9,10 @@ export class GetStatusUseCase {
     try {
       return await this.statusRepository.getStatus();
     } catch (error) {
-      console.error("Error during get status operation:", error);
+      if (error instanceof StatusError) {
+        throw error; // Re-throw the specific StatusError
+      }
+
       throw new Error("Internal server error during get status operation");
     }
   }
